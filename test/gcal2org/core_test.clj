1b29302de53e502fc12eaaf5be7e328fb094171c
Christian Berg <berg.christian@gmail.com>
2012-10-25 20:23:51 +0200

    Time range formatting is tested.

--- test/gcal2org/core_test.clj
+++ test/gcal2org/core_test.clj
@@ -9,3 +9,13 @@
 (fact "A date without time is correctly formatted."
       (event-date-time->org-date {"date" "2012-10-03"})
       => "<2012-10-03>")
+
+(fact "A timerange without times is correctly formatted."
+      (timerange {"start" {"date" "2012-01-01"}
+                  "end"   {"date" "2012-02-02"}})
+      => "<2012-01-01>--<2012-02-02>")
+
+(fact "A timerange with times is correctly formatted."
+      (timerange {"start" {"dateTime" "2012-01-01T10:00:00.000+02:00"}
+                  "end"   {"dateTime" "2012-01-01T11:30:00.000+02:00"}})
+      => "<2012-01-01 10:00>--<2012-01-01 11:30>")
